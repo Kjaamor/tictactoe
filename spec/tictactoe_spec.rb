8bde2d73c97b7523e39504c2f41cf4b75516b402
James O'Keefe <james.okeefe@Jamess-MBP.fritz.box>
2022-06-22 16:11:43 +0100

    Created a check win condition for same player complete row

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -4,4 +4,16 @@ describe 'Tic Tac Toe game' do
     it 'on start up create an empty 3x3 grid' do
         expect(create_grid).to eq ([["", "", ""],["", "", ""],["", "", ""]])
     end
+    describe 'win conditions' do
+        it 'return player 1 wins given three x\'s in any row' do
+            expect(check_win_condition([[:x,:x,:x],[:o,:o,""],["","",""]])).to eq("Player One Wins")
+        end
+        it 'return player 2 wins given three o\'s in any row' do
+            expect(check_win_condition([[:o,:o,:o],[:x,:x,""],[:x,"",""]])).to eq("Player Two Wins")
+        end
+    end
+
 end
+
+# update_grid(coordinates) (starting player is always x)
+# calculate_score([x,x,x],[o,x,""],["","",""]) => "Player 1 wins"
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,3 +1,13 @@
 def create_grid
     grid = [["", "", ""],["", "", ""],["", "", ""]]
 end
+
+def check_win_condition(grid)
+    grid.each do |row|
+        if row.uniq.size <= 1 && row.uniq[0] == :x
+            return 'Player One Wins'
+        elsif row.uniq.size <= 1 && row.uniq[0] == :o
+            return 'Player Two Wins'
+        end
+    end
+end
