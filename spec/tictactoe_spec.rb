af0c93240012609e05d038082ad109e631aba02b
James O'Keefe <james.okeefe@Jamess-MBP.fritz.box>
2022-07-05 15:58:10 +0100

    WIP: attempt to establish instance variables for tests.

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -11,8 +11,16 @@ describe "Tic Tac Toe game" do
         it "if player 1 inputs top left as their move choice on an empty grid, the grid reflects this" do
             expect(update_grid("1")).to eq ([["X", "", ""],["", "", ""],["", "", ""]])
         end
-        it "keeps track of updates to the grid" do
-            # Needs stuff putting in
-        end
+        # it "keeps track of updates to the grid" do
+        #     # game = TicTacToe.new
+        #     # game.update_grid("1")
+        #     # expect(game@grid).to eq ()
+        # end
     end
 end
+
+# describe 'tic tac toe' do
+#     before(:each) do
+#         @game = TicTacToe.new
+#     end
+# end
