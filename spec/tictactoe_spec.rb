f7265f064b288771de8f61de43798a3b215e12c8
Korinna Pinakoulakis <kpinakoulaki@gmail.com>
2022-07-06 11:12:41 +0100

    Turn grid into instance variable with a new TicTacToe class. Refactor grid creation.

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,26 +1,22 @@
 require_relative "../tictactoe.rb"
 
 describe "Tic Tac Toe game" do
+    before(:each) do
+        @game = TicTacToe.new
+    end
     describe "Grid Initialize" do
         it "on start up create an empty 3x3 grid" do
-            expect(create_grid).to eq ([["", "", ""],["", "", ""],["", "", ""]])
+            expect(@game.grid).to eq ([["", "", ""],["", "", ""],["", "", ""]])
         end
     end
 
     describe "Update grid based on player move" do
         it "if player 1 inputs top left as their move choice on an empty grid, the grid reflects this" do
-            expect(update_grid("1")).to eq ([["X", "", ""],["", "", ""],["", "", ""]])
+            expect(@game.update_grid("1")).to eq ([["X", "", ""],["", "", ""],["", "", ""]])
         end
-        # it "keeps track of updates to the grid" do
-        #     # game = TicTacToe.new
-        #     # game.update_grid("1")
-        #     # expect(game@grid).to eq ()
-        # end
+        it "keeps track of updates to the grid" do
+            @game.update_grid("1")
+            expect(@game.grid).to eq ([["X", "", ""],["", "", ""],["", "", ""]])
+        end
     end
 end
-
-# describe 'tic tac toe' do
-#     before(:each) do
-#         @game = TicTacToe.new
-#     end
-# end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,12 +1,22 @@
+class TicTacToe
+    
+    attr_accessor :grid
+    def initialize()
+        @grid = [["", "", ""],["", "", ""],["", "", ""]]
+    end
+
+    def update_grid(cell_id)
+        flat_grid = @grid.flatten
+        flat_grid[cell_id.to_i - 1] = "X"
+        @grid = flat_grid.each_slice(3).to_a
+    end
+end
+    
 def start_game
     puts "⚡️⚡️⚡️ WELCOME TO TIC TAC TOE ⚡️⚡️⚡️"
     puts "\nStandard rules apply. Cells are numbered as shown on the below grid. Please pick the cell by its number."
     puts "\n1|2|3\n4|5|6\n7|8|9"
     print "\nPlease pick a cell: "
-end
-
-def create_grid
-    grid = [["", "", ""],["", "", ""],["", "", ""]]
 end
 
 def check_win_condition(grid)
@@ -35,10 +45,4 @@ end
 
 def are_all_three_cells_the_same?(cells)
     cells.uniq.size <= 1 && cells[0] != ""
-end
-
-def update_grid(cell_id)
-    flat_grid = create_grid().flatten
-    flat_grid[cell_id.to_i - 1] = "X"
-    updated_grid = flat_grid.each_slice(3).to_a
 end
